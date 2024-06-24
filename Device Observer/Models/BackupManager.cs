9bc09bbcd13202fe915384d80853ee2f4213fb50
Alexander Pirogov <tenikssozdatel8971@gmail.com>
2024-06-24 21:51:36 +0300

    Fixed backup 2

--- Device Observer/Models/BackupManager.cs
+++ Device Observer/Models/BackupManager.cs
@@ -1,5 +1,5 @@
-﻿using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data.Entity;
+using System.IO;
 
 namespace Device_Observer.Models
 {
@@ -7,12 +7,17 @@ namespace Device_Observer.Models
     {
         public static void BackupDatabase(string backupFilePath)
         {
-            ApplicationContext.Instance.Database.ExecuteSqlCommand("EXEC BackupDataBase @backpath", new SqlParameter("@backath", backupFilePath));
+            ApplicationContext.Instance.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction, 
+                $@"BACKUP DATABASE [{Directory.GetCurrentDirectory()}\data\db_local.mdf] TO  " +
+                $@"DISK = N'{backupFilePath}' WITH NOFORMAT, NOINIT,  " +
+                $@"NAME = N'{backupFilePath}', SKIP, NOREWIND, NOUNLOAD,  STATS = 10");
         }
 
         public static void RestoreDatabase(string backupFilePath)
         {
-            ApplicationContext.Instance.Database.ExecuteSqlCommand("EXEC RestoreDatabase @backpath", new SqlParameter("@backath", backupFilePath));
+            ApplicationContext.Instance.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction,
+                $@"RESTORE DATABASE [{Directory.GetCurrentDirectory()}\data\db_local.mdf] FROM " +
+                $@"DISK = N'{backupFilePath}' WITH RECOVERY, REPLACE");
         }
     }
 }
